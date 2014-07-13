eebb8788609571419803256e55a3a03af69c8087
Rodney Degracia <rdegraci@elvenlabs.com>
2014-07-13 18:25:38 -0400

    Initial Commit

---
+++ NeudalTests/NeudalTests.m
@@ -0,0 +1,34 @@
+//
+//  NeudalTests.m
+//  NeudalTests
+//
+//  Created by Rodney Degracia on 7/13/14.
+//  Copyright (c) 2014 Venture Intellectual LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NeudalTests : XCTestCase
+
+@end
+
+@implementation NeudalTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
